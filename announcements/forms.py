27a46974989f7fc130032b4dabcbb5a4b7c44e05
Jim Barnes <james.barnes@ucf.edu>
2017-05-11 09:09:22 -0400

    Consolodated forms to one and added help_texts

--- announcements/forms.py
+++ announcements/forms.py
@@ -10,6 +10,19 @@ class AnnouncementForm(forms.ModelForm):
             'author': forms.HiddenInput,
             'status': forms.Select
         }
+        help_texts = {
+            'title': 'Enter a descriptive title',
+            'description': 'The primary content of your announcement',
+            'audience': 'Used for identifying the intended audience of this announcement',
+            'start_date': 'When the announcement should begin appearing in feeds and emails',
+            'end_date': 'When the announcement should stop appearing in feeds and emails',
+            'url': 'A url where additional information realted to your announcement can be found',
+            'contact_name': 'The name of the person or organization who can be contacted regarding this announcement',
+            'contact_phone': 'The phone number of the contact person or organization',
+            'contact_email': 'The email address of the contact person or organization',
+            'keywords': 'Enter each keyword followed by a comma (or by pressing tab)',
+            'posted_by': 'The name of the person or organization posting this announcement'
+        }
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super(AnnouncementForm, self).__init__(*args, **kwargs)

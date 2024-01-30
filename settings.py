53e86429b24d1b0baeb7046909e53fe0fdc2d48d
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-01-30 11:50:53 -0500

    add AWS App Runner to allowed hosts

--- settings.py
+++ settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+# Allow hosts to host this application
+ALLOWED_HOSTS = {
+    ".us-east-1.awsapprunner.com"
+}
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 

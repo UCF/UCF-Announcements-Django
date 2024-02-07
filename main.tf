e9c89ea3931d01aa1b05dbff9befb6d75b2950f3
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-02-07 11:02:58 -0500

    change main.tf to deploy ecs task definition

--- main.tf
+++ main.tf
@@ -12,3 +12,26 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+
+resource "aws_ecs_task_definition" "deploy_announcements_dev" {
+
+  family                = "announcements-deployments"
+  container_definitions = <<DEFINITION
+	[
+		{
+		"name": "ucf-announcements",
+		"image": "ucf-announcements:latest",
+		"portMappings": [
+		  {
+		    "containerPort": 8004,
+		    "hostPort": 80
+		  }
+		],
+		"memory": 512,
+		"cpu": 256
+		}
+	]	
+	DEFINITION
+}
+

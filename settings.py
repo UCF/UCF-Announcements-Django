29344d70314568a9c1a1c0ff23851c6e9c9b53f6
Jim Barnes <jimbarnesdeveloper@gmail.com>
2024-02-07 14:31:19 -0500

    Added pyenv.cfg files to getignore

--- settings.py
+++ settings.py
@@ -13,10 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Allow hosts to host this application
-ALLOWED_HOSTS = {
-    ".awsapprunner.com",
-    "localhost"
-}
+ALLOWED_HOSTS = []
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))

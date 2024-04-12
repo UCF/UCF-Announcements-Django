3f9ad4ea992f890fee366080b3b038394df0e8d1
Jim Barnes <jimbarnesdeveloper@gmail.com>
2024-04-12 16:48:21 -0400

    Updated to using the file aware env, which will also look for Docker style env files

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-env = environ.Env()
+env = environ.FileAwareEnv()
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 

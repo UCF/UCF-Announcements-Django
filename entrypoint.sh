c46840b4e4dc92320b71d05b5978e4b594deb610
ucfchandra <chandra.kethireddy@ucf.edu>
2024-11-25 14:49:24 -0500

    test superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,13 @@
 
 cd /app
 python manage.py migrate
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
 exec gunicorn --workers=2 --threads=4 --worker-class=gthread --bind '[::]:80' --worker-tmp-dir /dev/shm wsgi:application
